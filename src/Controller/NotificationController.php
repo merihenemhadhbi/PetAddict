772f436b67ab7a65deb29812b6b2a166bcca8ba9
Habib Zerai <hpiplolz@gmail.com>
2021-06-17 23:48:51 +0100

    notification services

---
+++ src/Controller/NotificationController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Notification;
+use App\Repository\NotificationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ *@Route("/api/notification")
+ */
+class NotificationController extends AbstractController
+{
+
+    private $notificationRepo;
+    private $serializer;
+    private $em;
+
+    public function __construct(NotificationRepository $notificationRepo, SerializerInterface $serializer, EntityManagerInterface $em)
+    {
+        $this->notificationRepo = $notificationRepo;
+        $this->serializer = $serializer;
+        $this->em = $em;
+    }
+
+
+
+
+    /**
+     * @Route("", name="get_user_notification" , methods = "GET")
+     */
+    public function getUserNotifications(): Response
+    {
+        $user_id = $this->getuser()->getEmail();
+        $allUserNotifications = $this->notificationRepo->findByToUser($user_id);
+        return new Response($this->serializer->serialize($allUserNotifications, 'json'), Response::HTTP_OK);
+    }
+   
+    /**
+     * @Route("", name="send_notification" , methods = "POST")
+     */
+    public function sendNotification(Request $request): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        $fromUser = $this->getuser()->getEmail();
+        $notification = new Notification();
+        $notification->setBody($data['body']);
+        $notification->setToUser($data['toUser']);
+        $notification->setFromUser($fromUser);
+        $this->em->persist($notification);
+        $this->em->flush();
+        return new Response($this->serializer->serialize($notification, 'json'), Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/{id}/read", name="read_notification" , methods = "POST")
+     */
+    public function readNotification($id): Response
+    {
+        $notification = $this->notificationRepo->find($id);
+        $notification->setVu(true);
+        $this->em->persist($notification);
+        $this->em->flush();
+        return new Response($this->serializer->serialize($notification, 'json'), Response::HTTP_OK);
+    }
+}
---
+++ src/Entity/Notification.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NotificationRepository;
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NotificationRepository::class) @ORM\HasLifecycleCallbacks
+ */
+class Notification
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $body;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $fromUser;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $toUser;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $createdBy;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $vu = false;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getFromUser(): ?string
+    {
+        return $this->fromUser;
+    }
+
+    public function setFromUser(string $fromUser): self
+    {
+        $this->fromUser = $fromUser;
+
+        return $this;
+    }
+
+    public function getToUser(): ?string
+    {
+        return $this->toUser;
+    }
+
+    public function setToUser(string $toUser): self
+    {
+        $this->toUser = $toUser;
+
+        return $this;
+    }
+
+    public function getCreatedBy(): ?string
+    {
+        return $this->createdBy;
+    }
+
+    public function setCreatedBy(string $createdBy): self
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+     /** @ORM\PrePersist */
+     public function prePersist()
+     {
+         $this->createdAt = new DateTime();
+         $this->createdBy = $this->fromUser;
+     }
+ 
+     /** @ORM\PreUpdate */
+     public function preUpdate()
+     {
+         $this->updatedAt = new DateTime();
+         $this->createdBy = $this->fromUser;
+     }
+
+     public function getVu(): ?bool
+     {
+         return $this->vu;
+     }
+
+     public function setVu(bool $vu): self
+     {
+         $this->vu = $vu;
+
+         return $this;
+     }
+}
